a1394a990520deacaca45e792e03013c22f98ba0
CloudNexus <abhighnagiri@gmail.com>
2026-05-06 17:22:03 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,16 @@
+
+---
+
+### `versions.tf`
+
+```hcl
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
